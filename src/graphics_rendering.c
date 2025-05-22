0d14cb24b4fd10bf84e82763e0eabb7962f5a407
Alexander Manasseh <alex.manasseh.ud@renesas.com>
2025-05-22 18:06:29 -0500

    Updated function comments Added/fixed timers for 4 and 1 bit CLUT Fixed memory overrun issue on 4 and 1 bit CLUT

--- src/graphics_rendering.c
+++ src/graphics_rendering.c
@@ -21,8 +21,7 @@ static void display_draw_clut (uint8_t * framebuffer);
 void CLUT_init(void);
 void RGB_init(void);
 
-/* Variables to store resolution information */
-uint16_t g_hz_size, g_vr_size;
+
 /* Variables used for buffer usage */
 uint32_t g_buffer_size, g_hstride;
 uint32_t * gp_single_buffer = NULL;
@@ -32,12 +31,7 @@ uint32_t * gp_frame_buffer  = NULL;
 //uint32_t timer_top = &g_time_counter.p_cfg->period_counts;
 
 
-/* Variables to store resolution information */
-uint16_t g_hz_size1, g_vr_size1;
 
-
-/* Variables used for buffer usage */
-uint32_t g_buffer_size1, g_hstride1;
 
 
 /* variables used for CLUT definition */
@@ -84,7 +78,7 @@ uint32_t clut_4[16] =
 
 uint32_t clut_1[2] =
 {
- CLUT_RED,
+     CLUT_RED,
      CLUT_PURPLE,
 };
 
@@ -110,32 +104,17 @@ display_clut_cfg_t clut_cfg =
 
 
 /*******************************************************************************************************************//**
- * @brief
+ * @brief      This initializes the CLUT drawing, sets the timers, commands the CLUT draw, and outputs timing on RTT
  *
- * @param[in]  [param in]
- * @retval     [return value]
+ * @param[in]  none
+ * @retval     none
  **********************************************************************************************************************/
 void CLUT_init(void)
 {
-    fsp_err_t err;
     volatile uint32_t timer_count_before, timer_count_reg_before, timer_count_after, timer_count_reg_after;
     volatile uint32_t timer_ms_decimal = 0;
 
-    //CLUT Setup
-    /* Get LCDC configuration */
-    g_hz_size1 = (g_display_cfg.input[1].hsize);
-    g_vr_size1 = (g_display_cfg.input[1].vsize);
-    g_hstride1 = (g_display_cfg.input[1].hstride); // DISPLAY_HSIZE_INPUT1;//
-    g_buffer_size1 = (uint32_t) (g_hz_size1 * g_vr_size1 * BYTES_PER_PIXEL_CLUT8);
-
-    //Update the CLUT table for the foreground (layer 2)
-    err = R_GLCDC_ClutUpdate(&g_display_ctrl, &clut_cfg, DISPLAY_FRAME_LAYER_2 );
-    /* Handle error */
-    if(err)
-    {
-        __BKPT();
-    }
-
+    //Prepare timer to measure the duration of rendering
     TimeCounter_CountReset();
     timer_count_before = TimeCounter_CurrentCountGet();
     timer_count_reg_before = TimeCounter_CurrentCountRegGet();
@@ -147,6 +126,7 @@ void CLUT_init(void)
     timer_count_after = TimeCounter_CurrentCountGet();
     timer_count_reg_after = TimeCounter_CurrentCountRegGet();
 
+    //combine 100ms inverval interrupt and counter register to get precise timing
     timer_ms_decimal = ((timer_count_after - timer_count_before)%10)*100 + ((timer_count_reg_after - timer_count_reg_before)*100)/g_time_counter.p_cfg->period_counts;
 
     APP_PRINT("\r\nCLUT Draw Time: %u.%u ms\tFrame Buffer Size: %u Bytes\r\n", (timer_count_after - timer_count_before)/10, timer_ms_decimal, sizeof(fb_foreground) );
@@ -154,23 +134,21 @@ void CLUT_init(void)
     TimeCounter_Start();
 }
 
+/*******************************************************************************************************************//**
+ * @brief      This initializes the RGB565 drawing, sets the timers, commands the CLUT draw, and outputs timing on RTT
+ *
+ * @param[in]  none
+ * @retval     none
+ **********************************************************************************************************************/
 void RGB_init(void)
 {
     volatile uint32_t timer_count_before, timer_count_reg_before, timer_count_after, timer_count_reg_after;
     volatile uint32_t timer_ms_decimal = 0;
 
-    /* Get LCDC configuration */
-    g_hz_size = (g_display_cfg.input[0].hsize);
-    g_vr_size = (g_display_cfg.input[0].vsize);
-    g_hstride = (g_display_cfg.input[0].hstride);
-
-    /* Initialize buffer pointers */
-    g_buffer_size = (uint32_t) (g_hz_size * g_vr_size * BYTES_PER_PIXEL_RGB565);
-    gp_single_buffer = (uint32_t*) g_display_cfg.input[0].p_base;
-
     /* Double buffer for drawing color bands with good quality */
     gp_double_buffer = gp_single_buffer + g_buffer_size;
 
+    //Prepare timer to measure the duration of rendering
     TimeCounter_Stop();
     TimeCounter_CountReset();
     timer_count_before = TimeCounter_CurrentCountGet();
@@ -185,6 +163,7 @@ void RGB_init(void)
     timer_count_after = TimeCounter_CurrentCountGet();
     timer_count_reg_after = TimeCounter_CurrentCountRegGet();
 
+    //combine 100ms inverval interrupt and counter register to get precise timing
     timer_ms_decimal = ((timer_count_after - timer_count_before)%10)*100 + ((timer_count_reg_after - timer_count_reg_before)*100)/g_time_counter.p_cfg->period_counts;
 
     APP_PRINT("\r\nRGB565 Draw Time: %u.%u ms\tFrame Buffer Size: %u Bytes\r\n", (timer_count_after - timer_count_before)/10, timer_ms_decimal, sizeof(fb_background) );
@@ -203,7 +182,15 @@ void RGB_init(void)
  **********************************************************************************************************************/
 static void display_draw_rgb565 (uint32_t * framebuffer)
 {
+    /* Variables to store resolution information */
+    uint16_t g_hz_size, g_vr_size;
+
     /* Draw buffer */
+    /* Get LCDC configuration */
+    g_hz_size = (g_display_cfg.input[0].hsize);
+    g_vr_size = (g_display_cfg.input[0].vsize);
+    g_hstride = (g_display_cfg.input[0].hstride);
+
     uint32_t color[COLOR_BAND_COUNT]= {BLUE, LIME, RED, BLACK, WHITE, YELLOW, AQUA, MAGENTA};
     uint16_t bit_width = g_hz_size / COLOR_BAND_COUNT;
 
@@ -227,8 +214,22 @@ static void display_draw_rgb565 (uint32_t * framebuffer)
  **********************************************************************************************************************/
 static void display_draw_clut (uint8_t * framebuffer)
 {
+//    fsp_err_t err;
+    /* Variables to store resolution information */
+    uint16_t g_hz_size1, g_vr_size1;
+
+    /* Variables used for buffer usage */
+    uint32_t g_hstride1;
+
+    //CLUT Setup
+    /* Get LCDC configuration */
+    g_hz_size1 = (g_display_cfg.input[1].hsize); //480
+    g_vr_size1 = (g_display_cfg.input[1].vsize); //854
+    g_hstride1 = (g_display_cfg.input[1].hstride); // DISPLAY_HSIZE_INPUT1;// 512
+
+
     /* Draw buffer */
-    uint16_t bit_width = (uint16_t)(g_vr_size1 / COLOR_BAND_COUNT_CLUT);
+    uint16_t bit_width = (uint16_t)(g_vr_size1 / COLOR_BAND_COUNT_CLUT); //56
     volatile uint8_t bit       = 0;
 
     for (uint32_t y = 0; y < g_vr_size1; y++)
@@ -241,7 +242,7 @@ static void display_draw_clut (uint8_t * framebuffer)
             bit = COLOR_BAND_COUNT_CLUT;
         }
 #elif CLUT_MODE == CLUT4
-        bit = ((y)/bit_width)<<4 | ((y)/bit_width);
+        bit = (uint8_t)(((y)/bit_width)<<4 | ((y)/bit_width));
 #elif CLUT_MODE == CLUT1
         if( 0 == y/bit_width)
         {
@@ -253,16 +254,28 @@ static void display_draw_clut (uint8_t * framebuffer)
         }
 #endif
 
+
+
+#if CLUT_MODE == CLUT8
         for (uint32_t x = 0; x < g_hz_size1; x++)
         {
 
             framebuffer[x] = bit;
         }
-#if CLUT_MODE == CLUT8
         framebuffer += g_hstride1;
 #elif CLUT_MODE == CLUT4
+        for (uint32_t x = 0; x < g_hz_size1/2; x++)
+        {
+
+            framebuffer[x] = bit;
+        }
         framebuffer += g_hstride1/2;
 #elif CLUT_MODE == CLUT1
+        for (uint32_t x = 0; x < g_hz_size1/8; x++)
+        {
+
+            framebuffer[x] = bit;
+        }
         framebuffer += g_hstride1/8;
 #endif
     }
